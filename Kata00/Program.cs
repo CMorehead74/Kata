bc989202e5498f45631776967db73600098bb705
JPG.COM\cmorehead <cmorehead@accusoft.com>
2017-11-28 16:51:59 -0500

    Fixed and completed version. TODO: refactor

--- Kata00/Program.cs
+++ Kata00/Program.cs
@@ -11,24 +11,37 @@ namespace Kata00
         static void Main(string[] args)
         {
             //TODO:
-            //take those 10 symbols and call function that will  verify RULES and remove dupicates
-
             //RULES: a valid hash value consists of five numbers and five lowercase letters in any order.
             //Return an array of valid hash values, and eliminate any duplicates
 
-            string[] hashValue = new String[] { "asdfiasdfg", "jsdfasdf14", "a212sdf345", "as00yud567", "as34asd567", "agsdgreast", "1dfg23asd4", "hjkjw98076", "fdsqweqrah", "12gh34gh1g" };
+            string[] hashValue = new String[] { "asdfiasdfg", "jsdfasdf14", "a212sdf345", "as00yud567",
+                "as34asd567", "agsdgreast", "1dfg23asd4", "hjkjw98076", "fdsqweqrah", "12gh34gh1g" };
 
+            List<String> outputString = new List<String>();
             for (int i = 0; i < hashValue.Length; i++)
-                StringSort(hashValue[i]);
+            {
+                if (StringSort(hashValue[i]))
+                {
+                    outputString.Add(hashValue[i]);
+                }
+            }
 
-            //TODO:
-            //output list of string items
+            String[] outputHashValue = outputString.ToArray();
 
-            List<string> StringSort(string pHashValue)
+            for (int i = 0; i < outputHashValue.Length; i++)
+            {
+                Console.WriteLine(outputHashValue[i]);
+            }
+
+            Console.ReadLine();
+
+            bool StringSort(string pHashValue)
             {
                 if (String.IsNullOrEmpty(pHashValue))
+                {
                     Console.WriteLine("Hash value empty");
-
+                    return false;
+                }
                 var countNumber = 0;
                 var countLetter = 0;
                 foreach (var item in pHashValue.ToCharArray())
@@ -39,19 +52,12 @@ namespace Kata00
                     if (char.IsLetter(item))
                         countLetter++;
                 }
-                List<string> output = new List<string>();
-                //TODO:
-                //looping thru the inputArray and adding index to the outputArray
                 if (countNumber == 5 && countLetter == 5)
                 {
-                    output.Add(pHashValue);
+                    return true;
                 }
                 else
-                    Console.WriteLine("No valid hash value found");
-
-                Console.ReadLine();
-
-                return output;
+                    return false;
             }
         }
     }
